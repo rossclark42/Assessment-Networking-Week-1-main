ad4f70dbf73a8312694c8017f76f566259cb4410
rossclark42 <ross.clark42@icloud.com>
2026-05-01 18:25:45 +0200

    Completed technical assessment

--- postcode_cli.py
+++ postcode_cli.py
@@ -1,7 +1,38 @@
 """A CLI application for interacting with the Postcode API."""
 
 from argparse import ArgumentParser
+from postcode_functions import get_postcode_completions, validate_postcode
+
+
+def main():
+    parser = ArgumentParser(description="Postcode utility tool.")
+    parser.add_argument("--mode", "-m", required=True, choices=["validate", "complete"],
+                        help="Select 'validate' or 'complete' mode.")
+    parser.add_argument("postcode", type=str,
+                        help="Postcode or partial postcode string.")
+    args = parser.parse_args()
+    postcode = args.postcode.strip().upper()
+    if args.mode == "validate":
+        try:
+            results = validate_postcode(postcode)
+            if results:
+                print(f"{postcode} is a valid postcode.")
+            else:
+                print(f"{postcode} is not a valid postcode.")
+        except Exception:
+            print(1)
+            print(f"{postcode} is not a valid postcode.")
+    elif args.mode == "complete":
+        try:
+            completions = get_postcode_completions(postcode)
+            if completions:
+                for postcode in completions[:5]:
+                    print(postcode)
+            else:
+                print(f"No matches for {postcode}.")
+        except Exception:
+            print(f"No matches for {postcode}.")
 
 
 if __name__ == "__main__":
-    pass
+    main()
--- postcode_functions.py
+++ postcode_functions.py
@@ -6,30 +6,88 @@ import json
 
 CACHE_FILE = "./postcode_cache.json"
 
+
 def load_cache() -> dict:
     """Loads the cache from a file and converts it from JSON to a dictionary."""
-    # This function is used in Task 3, you can ignore it for now.
-    ...
+    try:
+        with open(CACHE_FILE, "r", encoding="utf=8") as cache:
+            return json.load(cache)
+    except Exception:
+        return {}
 
 
 def save_cache(cache: dict):
     """Saves the cache to a file as JSON"""
-    # This function is used in Task 3, you can ignore it for now.
-    ...
-
+    with open(CACHE_FILE, "w", encoding="utf=8") as file:
+        json.dump(cache, file, indent=4)
 
 
 def validate_postcode(postcode: str) -> bool:
-    pass
+    """Function which will send a request to the postcode 
+    API and validate if it is a valid postcode"""
+    if not isinstance(postcode, str):
+        raise TypeError("Function expects a string.")
+    url_base = "https://api.postcodes.io/postcodes/"
+    URL = f"{url_base}{postcode}/validate"
+    response = req.get(URL, timeout=5)
+    if response.status_code != 200:
+        if response.status_code == 500:
+            raise req.RequestException("Unable to access API.")
+        return False
+    postcode_data = response.json()
+    if postcode_data["result"] == False:
+        return False
+    return True
 
 
 def get_postcode_for_location(lat: float, long: float) -> str:
-    pass
+    if not isinstance(lat, float) or not isinstance(long, float):
+        raise TypeError("Function expects two floats.")
+    url_base = "https://api.postcodes.io/postcodes?"
+    URL = f"{url_base}lon={long}&lat={lat}"
+    response = req.get(URL, timeout=5)
+    if response.status_code != 200:
+        if response.status_code == 500:
+            raise req.RequestException("Unable to access API.")
+        return False
+    long_lat_data = response.json()
+    if long_lat_data["result"] == None:
+        raise ValueError("No relevant postcode found.")
+    postcode = long_lat_data["result"][0]["postcode"]
+    return postcode
 
 
 def get_postcode_completions(postcode_start: str) -> list[str]:
-    pass
+    if not isinstance(postcode_start, str):
+        raise TypeError("Function expects a string.")
+    url_base = "https://api.postcodes.io/postcodes/"
+    URL = f"{url_base}{postcode_start}/autocomplete"
+    response = req.get(URL, timeout=5)
+    if response.status_code != 200:
+        if response.status_code == 500:
+            raise req.RequestException("Unable to access API.")
+        return False
+    postcode_completions = response.json()
+    if postcode_completions["result"] == None:
+        raise ValueError("No relevant postcode found.")
+    return postcode_completions["result"]
 
 
-def get_postcodes_details(postcodes: list[str]) -> dict:
-    pass
+def get_postcodes_details(postcodes: list[str]) -> list[dict]:
+    if not isinstance(postcodes, list) or not all(isinstance(p, str) for p in postcodes):
+        raise TypeError("Function expects a list of strings.")
+    url_base = "https://api.postcodes.io/postcodes/"
+    result = []
+    for postcode in postcodes:
+        postcode_f = postcode.replace(" ", "")
+        URL = f"{url_base}{postcode_f}"
+        response = req.get(URL, timeout=5)
+        if response.status_code != 200:
+            if response.status_code == 500:
+                raise req.RequestException("Unable to access API.")
+            return False
+        postcode_data = response.json()
+        # if postcode_data["result"] == None:
+        #     raise ValueError("No relevant postcode found.")
+        result.append[postcode_data["result"]]
+    return result
